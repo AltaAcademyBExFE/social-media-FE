1f09327d9423e86c76685019d6d81caa3f35a5b9
Annisaard <annisarahmadhani78@gmail.com>
2022-10-19 11:51:23 +0700

    rebase add slicing home

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { CiSearch } from "react-icons/ci";
 export const Navbar = () => {
   return (
-    <div className=" px-[4rem] bg-abu-100 ">
+    <div className=" px-[4rem] bg-abu-100 sticky">
       <h1>SocialUp</h1>
-      <input
-        id=""
+      {/* <input
         type="text"
         placeholder="Search"
-        className="w-[45%] h-[3rem] px-3 text-sm bg-abu-50 rounded-lg placeholder-abu-100 focus:outline-none border border-abu-120"
-      ></input>
+        className="w-[75%] h-[3rem] px-3 text-sm bg-abu-50 rounded-lg placeholder-abu-100 focus:outline-none border border-abu-120"
+      >
+        <CiSearch />
+      </input> */}
 
       <div className="flex">
         <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      abu: {
-        10: "#FFFFFF",
-        20: "#F3F0F0",
-        50: "#F3F3F3",
-        80: "#565555",
-        100: "#999999",
-        120: "#DCDCDC",
+    extend: {
+      colors: {
+        SocialUp: "#8CD6DB",
+        Grey: "#999999",
+        Linear: "#5AB0B6",
+        account: "#2B79C0",
+        abu: {
+          10: "#FFFFFF",
+          20: "#F3F0F0",
+          50: "#F3F3F3",
+          80: "#565555",
+          100: "#999999",
+          120: "#DCDCDC",
+        },
+        biru: {
+          50: "#5AB0B6",
+        },
+        hitam: {
+          50: "#282727",
+        },
       },
       biru: {
         50: "#5AB0B6",
@@ -18,11 +29,11 @@ module.exports = {
       hitam: {
         50: "#282727",
       },
-      "SocialUp" : "#8CD6DB",
-        "Grey" : "#999999",
-        "Linear" : "#5AB0B6",
-        "account" : "#2B79C0",
+      SocialUp: "#8CD6DB",
+      Grey: "#999999",
+      Linear: "#5AB0B6",
+      account: "#2B79C0",
     },
+    plugins: [],
   },
-  plugins: [],
 };
