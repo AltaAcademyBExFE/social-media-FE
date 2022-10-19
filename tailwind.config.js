159ea8867a17972627daee756f1984f612b47b17
Yamatsu69 <rikimuhammad3@gmail.com>
2022-10-19 13:58:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "SocialUp" : "#8CD6DB",
+        "Grey" : "#999999",
+        "Linear" : "#5AB0B6",
+        "account" : "#2B79C0"
+      }
+    },
+  },
+  plugins: [],
+}
